daf7970be138ebadbcc8cf4f9dd44ffe909c439e
Zoe Poeschl <zpoeschl@gmail.com>
2021-04-11 17:42:27 -0500

    started form event controls

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,7 @@ var tableData = data;
 // get reference to table body
 var tbody = d3.select("tbody");
 
+// ---- TABLE DATA ----
 // loop through data.js and move data into table
 data.forEach((sightingReport) => {
     var row = tbody.append("tr");
@@ -12,3 +13,20 @@ data.forEach((sightingReport) => {
         cell.text(value);
     });
 });
+
+
+// ---- FORM ----
+// select button
+var button = d3.select("#filter-btn");
+
+// select form
+var form = d3.select("#form");
+
+// create event handlers for clicking button or pressing enter
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// create function to run for both events
+function runEnter() {
+
+};
