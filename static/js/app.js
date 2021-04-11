815ce5451bcf4d054b93962c0ec6d5dcba4cf65b
Zoe Poeschl <zpoeschl@gmail.com>
2021-04-11 17:33:03 -0500

    table data filled in from data.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// get reference to table body
+var tbody = d3.select("tbody");
+
+// loop through data.js and move data into table
+data.forEach((sightingReport) => {
+    var row = tbody.append("tr");
+    Object.entries(sightingReport).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
