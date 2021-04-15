5942f93e59802837d1a976fadc93e6970642beb9
Zoe Poeschl <zpoeschl@gmail.com>
2021-04-15 12:41:17 -0500

    added favicon

--- static/js/app.js
+++ static/js/app.js
@@ -54,14 +54,7 @@ function runEnter() {
     for (i = 1; i < tr.length; i++) {
         tr[i].style.display = "none";
 
-        td = tr[i].getElementsByTagName("td");
-        for (var j = 0; j < td.length; j++) {
-            cell = tr[i].getElementsByTagName("td")[j];
-            if (cell) {
-                // if (cell.innerText.indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                    break;
-                };
-            };
-        };
+        tr[i].style.display = "";
+        break;
     };
+};
